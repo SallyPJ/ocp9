287080d2a21400d33fbc9f9c69a9097d02c1b9f3
Sally PJ <polonio.sally@gmail.com>
2024-11-13 13:20:37 +0100

    First commit Ajout de la DB chore: masquage de la secret key dans settings.py feat: création du LoginForm dans forms.py feat: ajout de la vue login_page pour la connexion des utilisateurs feat: création du gabarit login.html pour la page de connexion feat: ajout de l'URL pour la vue login_page dans urls.py feat: ajout de l'URL logout dans urls.py feat: ajout du lien de déconnexion dans base.html feat: ajout de la vue logout_user pour la déconnexion des utilisateurs feat: ajout de la vue et du gabarit pour la page d'accueil restreinte feat: redirection vers la page d'accueil après connexion feat: ajout du décorateur login_required sur la page d'accueil refactor : Vue de la page d'accueil basée sur une classe (au lieu d'une fonction)

--- BooksReview/BooksReview/settings.py
+++ BooksReview/BooksReview/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+LOGIN_URL = 'login'
--- BooksReview/authentication/views.py
+++ BooksReview/authentication/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.views.generic import View
 from . import forms
 
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+class LoginPage(View):
+    form_class = forms.LoginForm
+    template_name = 'authentication/login.html'
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(
+            request, self.template_name, context={'form': form, 'message': message})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        message = ''
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            message = 'Identifiants invalides.'
+        return render(
+            request, self.template_name, context={'form': form, 'message': message})
 
 def login_page(request):
     form = forms.LoginForm()
@@ -15,8 +43,7 @@ def login_page(request):
             )
             if user is not None:
                 login(request, user)
-                message = f'Bonjour, {user.username}! Vous êtes connecté.'
-            else:
-                message = 'Identifiants invalides.'
+                return redirect('home')
+            message = 'Identifiants invalides.'
     return render(
         request, 'authentication/login.html', context={'form': form, 'message': message})
