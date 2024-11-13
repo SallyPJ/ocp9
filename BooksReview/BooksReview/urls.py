eeace1afe95b03a2d9dcbb82897f3880142ec3b0
Sally PJ <polonio.sally@gmail.com>
2024-11-13 15:10:26 +0100

    feat : ajout du changement de mot de passe via les vues génériques refactor : utilisation d'une vue générique pour le Login

--- BooksReview/BooksReview/urls.py
+++ BooksReview/BooksReview/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 
 import authentication.views
@@ -23,7 +25,18 @@ import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.LoginPage.as_view(), name='login'),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+         name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+         name='password_change'
+     ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+         name='password_change_done'
+         ),
     path('home/', blog.views.home, name='home'),
 ]
--- BooksReview/authentication/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d\'utilisateur')
-    password = forms.CharField(max_length=63, label='Mot de passe', widget=forms.PasswordInput)
