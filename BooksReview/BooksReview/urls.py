e165f0e1aab015e534d2e7994040ed0209f573fe
Sally PJ <polonio.sally@gmail.com>
2024-11-13 15:25:27 +0100

    feat : ajout du sign up

--- BooksReview/BooksReview/urls.py
+++ BooksReview/BooksReview/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
          name='password_change_done'
          ),
     path('home/', blog.views.home, name='home'),
+    path('signup/', authentication.views.signup_page, name='signup'),
 ]
