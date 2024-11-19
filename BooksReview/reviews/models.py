2bb6222b81963ec2c3bda2aa0e2289a45577256e
Sally PJ <polonio.sally@gmail.com>
2024-11-19 14:44:08 +0100

    feat: ajout du resized_image sur les photos

--- BooksReview/reviews/models.py
+++ BooksReview/reviews/models.py
@@ -2,12 +2,24 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
 
+from PIL import Image
+
 
 class Photo(models.Model):
     image = models.ImageField()
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800,800)
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Ticket(models.Model):
