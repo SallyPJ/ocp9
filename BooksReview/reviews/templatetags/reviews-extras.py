39b6ed5918517e0db5632ea81eb55fdc0c4ce092
Sally PJ <polonio.sally@gmail.com>
2024-11-27 22:52:48 +0100

    feat : balises personnalisées pour le flux

--- BooksReview/reviews/templatetags/reviews-extras.py
+++ BooksReview/reviews/templatetags/reviews-extras.py
@@ -1,8 +1,14 @@
 from django import template
 
-register = template.Library()
-
-
-@register.filter
+'''@register.filter
 def model_type(value):
     return type(value).__name__
+    '''
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if context['user'] == user:
+        return 'Vous avez'
+    return f'{user.username} a '
