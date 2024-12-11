5b1dd49b92220530974f8fc6122397072a8fff55
Sally PJ <polonio.sally@gmail.com>
2024-12-11 17:57:01 +0100

    refactor : partage de la modale de suppression de critique entre les tickets et review style : maj du css (@media)

--- LitReview/LitReview/urls.py
+++ LitReview/LitReview/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth.views import (
-    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
+    LoginView, LogoutView)
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
